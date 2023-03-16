51ae16d518f1098c34ff0cf87e66f74b9aca9e3d
FaeruzzQ <faeruzq@students.amikom.ac.id>
2023-03-16 15:02:19 +0700

    menambahkan perkalian dan pembagian

--- Calculator.cs
+++ Calculator.cs
@@ -5,8 +5,10 @@ class Calculator
 		int a = 10;
 		int b = 6;
 		
-		Console.WriteLine("Hasil Penambahan : {0} + {1}", a, n, Penambahan(a, b));
-		Console.WriteLine("Hasil Pengurangan : {0} + {1}", a, n, Pengurangan(a, b));
+		Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, n, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan : {0} + {1} = {2}", a, n, Pengurangan(a, b));
+		Console.WriteLine("Hasil Perkalian : {0} * {1} = {2}", a, n, Perkalian(a, b));
+		Console.WriteLine("Hasil Pembagian : {0} + {1} = {2}", a, n, Pembagian(a, b));
 		
 		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.ReadKey
@@ -20,5 +22,15 @@ class Calculator
 	static int Pengurangan (int a, int b)
 	{
 		return a - b;
+	}
+	
+	static int Perkalian (int a, int b) 
+	{
+		return a * b;
+	}
+	
+	static int Pembagian (int a, int b)
+	{
+		return a/b;
 	}
 }
